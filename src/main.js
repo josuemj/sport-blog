8a9bb7a1176c2e79d15fea4eba22a20363098860
josuemj <josuemj456gmail.com>
2024-03-10 20:08:35 -0700

    feat: main.js error handling for posts implemented

--- src/main.js
+++ src/main.js
@@ -8,9 +8,18 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/posts', async (req, res) => {
-    const posts = await getAllPosts();
-    console.log(posts);
-    res.json(posts);
+    try {
+        const posts = await getAllPosts();
+        console.log(posts);
+        if(posts.length > 0){
+            res.status(200).json(posts);
+        } else {
+            res.status(404).json({message: "No posts found"});
+        }
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 const port = 5000
