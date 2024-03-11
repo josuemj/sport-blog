80b086996f3c06adc55c6e386200ad17561fab29
josuemj <josuemj456gmail.com>
2024-03-10 20:22:26 -0700

    feat: post implemented with error status handled

--- src/db.js
+++ src/db.js
@@ -4,3 +4,10 @@ export async function getAllPosts() {
  const [rows] = await conn.query('SELECT * FROM blog_posts')
  return rows
 }
+
+export async function createPost({ title, content, video }) {
+    const result = await conn.query(
+      'INSERT INTO blog_posts (title, content, video) VALUES (?, ?, ?)',
+      [title, content, video]
+    );
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { getAllPosts } from './db.js';
+import { getAllPosts, createPost } from './db.js';
 
 const app = express()
+app.use(express.json());
+
 
 app.get('/', async (req, res) => {
   res.send('HELLO FROM MY SERVER')
@@ -21,6 +23,21 @@ app.get('/posts', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+app.post('/posts', async (req, res) => {
+    try {
+        const { title, content, video } = req.body;
+        if (!title || !content) {
+            return res.status(400).json({ error: 'Title and content are required.' });
+        }
+        const post = await createPost({ title, content, video });
+        res.status(200).json(post);
+    } catch (error) {
+        console.error('Error creating post:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 const port = 5000
 
