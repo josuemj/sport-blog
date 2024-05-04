b535c1f0cf1626de50b4db7f03b6b931383cf916
josuemj <josuemj456@gmail.com>
2024-05-04 13:28:29 -0600

    swothced port test to 22397

--- src/main.js
+++ src/main.js
@@ -93,7 +93,7 @@ app.delete('/posts/:postId', async (req, res) => {
     }
 });
 
-const port = 5000
+const port = 22397
 
 app.listen(port, () => {
   console.log(`Server listening at http://127.0.0.1:${port}`)
